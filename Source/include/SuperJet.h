e1c3a1ae93a9b37b4df690a7143f66202659e302
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:00 +0200

    SuperJet global header file

---
+++ Source/include/SuperJet.h
@@ -0,0 +1,14 @@
+#ifndef SUPERJET_JAVA_H
+#define SUPERJET_JAVA_H
+
+#include "Java/Types.h"
+
+#include "Java/Archive/Archive.h"
+#include "Java/Archive/AttributeInfo.h"
+#include "Java/Archive/Class.h"
+#include "Java/Archive/ConstantPool.h"
+#include "Java/Archive/ConstantPoolEntry.h"
+#include "Java/Archive/FieldInfo.h"
+#include "Java/Archive/MethodInfo.h"
+
+#endif //SUPERJET_JAVA_H
