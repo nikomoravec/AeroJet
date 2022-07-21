c4da2f3fb88ba8ae2dfe03397af1b3ebf65a0fd7
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:01 +0200

    Updated general include file

--- Source/include/SuperJet.h
+++ Source/include/SuperJet.h
@@ -3,12 +3,19 @@
 
 #include "Java/Types.h"
 
+#include "Java/Archive/Attributes/Code.h"
+#include "Java/Archive/Attributes/LineNumberTable.h"
+
 #include "Java/Archive/Archive.h"
 #include "Java/Archive/AttributeInfo.h"
 #include "Java/Archive/ClassInfo.h"
 #include "Java/Archive/ConstantPool.h"
 #include "Java/Archive/ConstantPoolEntry.h"
+#include "Java/Archive/FieldDescriptor.h"
 #include "Java/Archive/FieldInfo.h"
+#include "Java/Archive/MethodDescriptor.h"
 #include "Java/Archive/MethodInfo.h"
+
+#include "Java/JVM/Runtime/Operations.h"
 
 #endif //SUPERJET_JAVA_H
