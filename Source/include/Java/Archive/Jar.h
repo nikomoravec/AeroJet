0fb7a316a5de03442c80bf77b523af2b9dd3ecae
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:02 +0200

    improvements for jar archives reader

--- Source/include/Java/Archive/Jar.h
+++ Source/include/Java/Archive/Jar.h
@@ -5,20 +5,50 @@
 #include "zip.h"
 
 #include <filesystem>
+#include <optional>
 #include <strstream>
+#include <vector>
 
 namespace SuperJet::Java::Archive
 {
     class Jar
     {
     public:
+        class Entry
+        {
+        public:
+            Entry(zip_t* inZip, const std::filesystem::path& path);
+            Entry(zip_t* inZip, ssize_t inIndex);
+            ~Entry();
+
+            std::stringstream read() const;
+            const std::string& getName() const;
+            ssize_t getIndex() const;
+            bool isDirectory() const;
+
+        protected:
+            Entry(zip_t* inZip);
+
+        protected:
+            zip_t* zip;
+
+            std::string name;
+            ssize_t index = 0;
+        };
+
+    public:
         Jar(const std::filesystem::path& path);
+        Jar(const Jar& other);
         ~Jar();
 
-        bool contains(const std::filesystem::path& path) const;
-        std::stringstream read(const std::filesystem::path& path) const;
+        Jar::Entry open(const std::filesystem::path& path) const;
+        Jar::Entry open(ssize_t index) const;
+
+        const std::filesystem::path& getLocation() const;
+        ssize_t count() const;
 
     protected:
+        std::filesystem::path location;
         zip_t* zip;
     };
 }
--- Source/source/Java/Archive/Jar.cpp
+++ Source/source/Java/Archive/Jar.cpp
@@ -1,10 +1,76 @@
 #include "Java/Archive/Jar.h"
 #include "Compiler/Exceptions/FileNotFoundException.h"
 #include <sstream>
+#include "spdlog/spdlog.h"
 
 namespace SuperJet::Java::Archive
 {
-    Jar::Jar(const std::filesystem::path& path) : zip(nullptr)
+    Jar::Entry::Entry(zip_t* inZip) : zip(inZip)
+    {
+        if (zip == nullptr)
+        {
+            throw Compiler::RuntimeException("zip can not be null!");
+        }
+    }
+
+    Jar::Entry::Entry(zip_t* inZip, const std::filesystem::path& path) : Entry(inZip)
+    {
+        const int open = zip_entry_open(zip, path.c_str());
+        if (open != 0)
+        {
+            throw Compiler::RuntimeException(fmt::format("Failed to read entry \"{}\"! Error: {}", path.string(), open));
+        }
+
+        name = zip_entry_name(zip);
+        index = zip_entry_index(zip);
+    }
+
+    Jar::Entry::Entry(zip_t* inZip, ssize_t inIndex) : Entry(inZip)
+    {
+        index = inIndex;
+
+        const int open = zip_entry_openbyindex(zip, index);
+        if (open != 0)
+        {
+            throw Compiler::RuntimeException(fmt::format("Failed to read entry at index \"{}\"! Error: {}", inIndex, open));
+        }
+
+        name = zip_entry_name(zip);
+    }
+
+    Jar::Entry::~Entry()
+    {
+        zip_entry_close(zip);
+    }
+
+    std::stringstream Jar::Entry::read() const
+    {
+        const unsigned long long bufferSize = zip_entry_size(zip);
+        auto* const buffer = static_cast<unsigned char*>(calloc(sizeof(unsigned char), bufferSize));
+        zip_entry_noallocread(zip, buffer, bufferSize);
+
+        std::stringstream ss;
+        std::move(&buffer[0], &buffer[bufferSize], std::ostream_iterator<unsigned char>(ss));
+
+        return ss;
+    }
+
+    const std::string& Jar::Entry::getName() const
+    {
+        return name;
+    }
+
+    ssize_t Jar::Entry::getIndex() const
+    {
+        return index;
+    }
+
+    bool Jar::Entry::isDirectory() const
+    {
+        return zip_entry_isdir(zip);
+    }
+
+    Jar::Jar(const std::filesystem::path& path) : zip(nullptr), location(path)
     {
         if (!std::filesystem::exists(path))
         {
@@ -14,43 +80,44 @@ namespace SuperJet::Java::Archive
         zip = zip_open(path.c_str(), 0, 'r');
         if (zip == nullptr)
         {
-            throw Compiler::RuntimeException(fmt::format("Failed to open JAR archive \"{}\""));
+            throw Compiler::RuntimeException(fmt::format("Failed to open JAR archive \"{}\"", path.string()));
+        }
+    }
+
+    Jar::Jar(const Jar& other)
+    {
+        this->location = other.location;
+        this->zip = zip_open(location.c_str(), 0, 'r');
+
+        if (zip == nullptr)
+        {
+            throw Compiler::RuntimeException(fmt::format("Failed to re-open (copy) Jar \"{}\"", location.string()));
         }
     }
 
     Jar::~Jar()
     {
         zip_close(zip);
+        zip = nullptr;
     }
 
-    bool Jar::contains(const std::filesystem::path& path) const
+    const std::filesystem::path& Jar::getLocation() const
     {
-        const int open = zip_entry_open(zip, path.c_str());
-        zip_entry_close(zip);
-
-        return open == 0;
+        return location;
     }
 
-    std::stringstream Jar::read(const std::filesystem::path& path) const
+    ssize_t Jar::count() const
     {
-        const int open = zip_entry_open(zip, path.c_str());
+        return zip_entries_total(zip);
+    }
 
-        if (open == 0)
-        {
-            const unsigned long long bufferSize = zip_entry_size(zip);
-            auto* const buffer = static_cast<unsigned char*>(calloc(sizeof(unsigned char), bufferSize));
-            zip_entry_noallocread(zip, buffer, bufferSize);
+    Jar::Entry Jar::open(const std::filesystem::path& path) const
+    {
+        return {zip, path};
+    }
 
-            std::stringstream ss;
-            std::move(&buffer[0], &buffer[bufferSize], std::ostream_iterator<unsigned char>(ss));
-
-            zip_entry_close(zip);
-
-            return ss;
-        }
-
-        zip_entry_close(zip);
-
-        throw Compiler::RuntimeException(fmt::format("Failed to read entry \"{}\"! Error: {}", path.string(), open));
+    Jar::Entry Jar::open(ssize_t index) const
+    {
+        return {zip, index};
     }
 }
