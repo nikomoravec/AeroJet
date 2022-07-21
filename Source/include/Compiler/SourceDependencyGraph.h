44681dc01a6155ed179e198132f2ff06c4b1ee9c
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:02 +0200

    Dependency graph topology sort

--- Source/include/Compiler/SourceDependencyGraph.h
+++ Source/include/Compiler/SourceDependencyGraph.h
@@ -4,6 +4,7 @@
 #include "Java/Archive/ClassInfo.h"
 #include "dglib/digraph.hh"
 #include <string>
+#include <queue>
 
 namespace SuperJet::Compiler
 {
@@ -15,6 +16,43 @@ namespace SuperJet::Compiler
 
     class SourceDependencyGraph : public digraph<std::shared_ptr<Java::Archive::ClassInfo>, ConnectionType>
     {
+    public:
+        std::queue<std::shared_ptr<Java::Archive::ClassInfo>> topology()
+        {
+            std::set<std::shared_ptr<Java::Archive::ClassInfo>> visited;
+            std::queue<std::shared_ptr<Java::Archive::ClassInfo>> result;
+
+            for(const std::shared_ptr<Java::Archive::ClassInfo>& node : nodes())
+            {
+                if (!visited.contains(node))
+                {
+                    topologySort(node, visited, result);
+                }
+            }
+            
+            return result;
+        }
+
+    private:
+        void topologySort(const std::shared_ptr<Java::Archive::ClassInfo>& node, std::set<std::shared_ptr<Java::Archive::ClassInfo>>& visited, std::queue<std::shared_ptr<Java::Archive::ClassInfo>>& result)
+        {
+            visited.emplace(node);
+
+            const std::map<std::shared_ptr<Java::Archive::ClassInfo>, ConnectionType>& nodeConnections = connections(node);
+            for (const auto& connection : nodeConnections)
+            {
+                ConnectionType connectionType = connection.second;
+                if (connectionType == ConnectionType::HARD)
+                {
+                    if(!visited.contains(connection.first))
+                    {
+                        topologySort(connection.first, visited, result);
+                    }
+                }
+            }
+
+            result.push(node);
+        }
     };
 }
 
