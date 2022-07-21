c46be16f066e4c2e14cee5024e640a4f00aaf3d1
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:00 +0200

    AttributeInfo implementation

---
+++ Source/include/Java/Archive/AttributeInfo.h
@@ -0,0 +1,26 @@
+#ifndef SUPERJET_ATTRIBUTEINFO_H
+#define SUPERJET_ATTRIBUTEINFO_H
+
+#include <vector>
+
+#include "Java/Types.h"
+
+namespace SuperJet::Java::Archive
+{
+    class AttributeInfo
+    {
+    public:
+        AttributeInfo(const JVM::u2 inAttributeIndex, const std::vector<JVM::u1>& inInfo) : attributeNameIndex(inAttributeIndex), info(inInfo)
+        {
+        }
+
+        JVM::u2 getAttributeNameIndex() const { return attributeNameIndex; }
+        JVM::u2 getSize() const { return info.size(); }
+
+    protected:
+        JVM::u2 attributeNameIndex;
+        std::vector<JVM::u1> info;
+    };
+}
+
+#endif //SUPERJET_ATTRIBUTEINFO_H
