e0ab3300efa1e55011f63e2675063d23624b34cf
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:01 +0200

    field and method descriptors scructures and resolve mechanism

---
+++ Source/source/Java/Archive/FieldDescriptor.cpp
@@ -0,0 +1,80 @@
+#include "Java/Archive/FieldDescriptor.h"
+#include "fmt/format.h"
+
+namespace SuperJet::Java::Archive
+{
+    FieldDescriptor::FieldDescriptor(const std::string& inDescriptor) : rawLiteral(inDescriptor), type(resolve())
+    {
+        if (rawLiteral.empty())
+        {
+            throw std::runtime_error("Field descriptor could not be empty!");
+        }
+    }
+
+    std::optional<FieldDescriptor> FieldDescriptor::getUnderlyingType() const
+    {
+        if (type == FieldType::ARRAY)
+        {
+            return FieldDescriptor(std::string(++rawLiteral.begin(), rawLiteral.end()));
+        }
+
+        return std::nullopt;
+    }
+
+    std::optional<std::string> FieldDescriptor::getClassName() const
+    {
+        if (type == FieldType::CLASS)
+        {
+            return std::string(++rawLiteral.begin(), --rawLiteral.end());
+        }
+
+        return std::nullopt;
+    }
+
+    FieldDescriptor::FieldType FieldDescriptor::resolve()
+    {
+        const char first = *rawLiteral.begin();
+        if (rawLiteral.size() > 1)
+        {
+            if (first == static_cast<char>(FieldType::CLASS))
+            {
+                if (rawLiteral[rawLiteral.size() - 1] == FIELD_TYPE_CLASS_END_TOKEN)
+                {
+                    return FieldType::CLASS;
+                }
+            }
+
+            if (first == static_cast<char>(FieldType::ARRAY))
+            {
+                return FieldType::ARRAY;
+            }
+        }
+
+        if (rawLiteral.size() != 1)
+        {
+            throw std::runtime_error(fmt::format("Incorrect Field Descriptor literal: \"{}\"", rawLiteral));
+        }
+
+        switch(first)
+        {
+            case static_cast<char>(FieldType::BYTE):
+                return FieldType::BYTE;
+            case static_cast<char>(FieldType::CHAR):
+                return FieldType::CHAR;
+            case static_cast<char>(FieldType::DOUBLE):
+                return FieldType::DOUBLE;
+            case static_cast<char>(FieldType::FLOAT):
+                return FieldType::FLOAT;
+            case static_cast<char>(FieldType::INTEGER):
+                return FieldType::INTEGER;
+            case static_cast<char>(FieldType::LONG):
+                return FieldType::LONG;
+            case static_cast<char>(FieldType::SHORT):
+                return FieldType::SHORT;
+            case static_cast<char>(FieldType::BOOLEAN):
+                return FieldType::BOOLEAN;
+            default:
+                throw std::runtime_error(fmt::format("Can not resolve type of field descriptor: \"{}\"", rawLiteral));
+        }
+    }
+}
