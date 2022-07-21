a260b2c6404d0e74fdd7adc780ef7c200e09830f
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:02 +0200

    Fixed missed format argument in OperationNotSupportedException

--- Source/include/Compiler/Exceptions/OperationNotSupportedException.h
+++ Source/include/Compiler/Exceptions/OperationNotSupportedException.h
@@ -9,7 +9,7 @@ namespace SuperJet::Compiler
     class OperationNotSupportedException : public NotSupportedException
     {
     public:
-        OperationNotSupportedException(Java::JVM::Runtime::OperationCode opCode) : NotSupportedException(fmt::format("Operation {:#06x} not supported!"))
+        OperationNotSupportedException(Java::JVM::Runtime::OperationCode opCode) : NotSupportedException(fmt::format("Operation {:#06x} not supported!", static_cast<SuperJet::Java::JVM::u1>(opCode)))
         {
         }
     };
