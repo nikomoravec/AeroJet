1ac69b2d75c9f674815c1eb0ffaedfbf632fa64f
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-07-21 22:14:01 +0200

    access flags for MethodInfo

--- Source/include/Java/Archive/MethodInfo.h
+++ Source/include/Java/Archive/MethodInfo.h
@@ -9,13 +9,29 @@ namespace SuperJet::Java::Archive
     class MethodInfo
     {
     public:
+        enum class AccessFlags : JVM::u2
+        {
+            ACC_PUBLIC = 0x0001,
+            ACC_PRIVATE = 0x0002,
+            ACC_PROTECTED = 0x0004,
+            ACC_STATIC = 0x0008,
+            ACC_FINAL = 0x0010,
+            ACC_SYNCHRONIZED = 0x0020,
+            ACC_BRIDGE = 0x0040,
+            ACC_VARARGS = 0x0080,
+            ACC_NATIVE = 0x0100,
+            ACC_ABSTRACT = 0x0400,
+            ACC_STRICT = 0x0800,
+            ACC_SYNTHETIC = 0x1000
+        };
+
         MethodInfo(const JVM::u2 inAccessFlags, const JVM::u2 inNameIndex, const JVM::u2 inDescriptorIndex, const std::vector<AttributeInfo>& inAttributes) : accessFlags(inAccessFlags), nameIndex(inNameIndex), descriptorIndex(inDescriptorIndex), attributes(inAttributes)
         {
         }
 
-        JVM::u2 getAccessFlags() const
+        AccessFlags getAccessFlags() const
         {
-            return accessFlags;
+            return static_cast<AccessFlags>(accessFlags);
         }
 
         JVM::u2 getNameIndex() const
