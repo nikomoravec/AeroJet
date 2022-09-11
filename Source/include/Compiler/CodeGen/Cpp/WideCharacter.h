04289e9bbfc39666e9a2549dba99aa604026e2dd
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2022-09-11 21:43:03 +0200

    removed wchar_t parameter from WideChar codegenclass

--- Source/include/Compiler/CodeGen/Cpp/WideCharacter.h
+++ Source/include/Compiler/CodeGen/Cpp/WideCharacter.h
@@ -4,40 +4,26 @@
 #include "Java/Types.h"
 #include "Compiler/CodeGen/Node.h"
 
-#include <string>
-#include <optional>
-
 namespace SuperJet::Compiler::CodeGen::Cpp
 {
     class Character : public SuperJet::Compiler::CodeGen::Node
     {
     public:
-        Character() : value(std::nullopt)
+        Character() : value(0x00)
         {
         }
 
-        Character(int32_t inValue) : value({static_cast<wchar_t>(inValue)})
-        {
-        }
-
-        Character(wchar_t inValue) : value({inValue})
+        Character(int32_t inValue) : value(inValue)
         {
         }
 
         virtual void dump(std::ostream& outputStream) override
         {
-            if (!value.has_value())
-            {
-                outputStream << static_cast<wchar_t>(0x00);
-            }
-            else
-            {
-                outputStream << "L'" << value.value() << "'";
-            }
+            outputStream << value;
         }
 
     protected:
-        std::optional<wchar_t> value;
+        int32_t value;
     };
 }
 
