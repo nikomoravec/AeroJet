f9e161b236fdde9088c38a92aeaa127a1b398baf
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2023-01-01 15:38:49 +0100

    License update

--- Source/AeroJet/source/Java/ClassFile/Attributes/SourceFile.cpp
+++ Source/AeroJet/source/Java/ClassFile/Attributes/SourceFile.cpp
@@ -1,7 +1,7 @@
 /*
  * SourceFile.cpp
  *
- * Copyright © 2022 AeroJet Developers. All Rights Reserved.
+ * Copyright © 2023 AeroJet Developers. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the “Software”), to deal
