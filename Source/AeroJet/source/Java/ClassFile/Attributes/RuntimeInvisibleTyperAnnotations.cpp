08032d49bd7b44304009233d57a57ca8ad85d68d
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2023-02-18 01:04:48 +0100

    RuntimeInvisibleTypeAnnotations support

--- Source/AeroJet/source/Java/ClassFile/Attributes/RuntimeInvisibleTyperAnnotations.cpp
+++ Source/AeroJet/source/Java/ClassFile/Attributes/RuntimeInvisibleTyperAnnotations.cpp
@@ -1,25 +1,25 @@
 /*
-* RuntimeInvisibleTyperAnnotations.hpp
-*
-* Copyright © 2023 AeroJet Developers. All Rights Reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the “Software”), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
-* OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+ * RuntimeInvisibleTyperAnnotations.hpp
+ *
+ * Copyright © 2023 AeroJet Developers. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "Java/ClassFile/Attributes/RuntimeInvisibleTypeAnnotations.hpp"
@@ -27,7 +27,8 @@
 
 namespace AeroJet::Java::ClassFile
 {
-    RuntimeInvisibleTyperAnnotations::RuntimeInvisibleTyperAnnotations(const ConstantPool& constantPool, const AttributeInfo& attributeInfo) :
+    RuntimeInvisibleTyperAnnotations::RuntimeInvisibleTyperAnnotations(const ConstantPool&  constantPool,
+                                                                       const AttributeInfo& attributeInfo) :
         Attribute(constantPool, attributeInfo, RUNTIME_INVISIBLE_TYPE_ANNOTATIONS_ATTRIBUTE_NAME)
     {
         const u2 numAnnotations = Stream::Reader::read<u2>(m_infoDataStream, Stream::ByteOrder::INVERSE);
@@ -40,4 +41,4 @@ namespace AeroJet::Java::ClassFile
             m_annotations.emplace_back(typeAnnotation);
         }
     }
-}
+} // namespace AeroJet::Java::ClassFile
