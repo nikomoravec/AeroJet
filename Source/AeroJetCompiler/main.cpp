46bb074c094de084375c0cdd7e7e48b793f76d7f
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2023-05-28 17:41:56 +0200

    compaler entry point update

--- Source/AeroJetCompiler/main.cpp
+++ Source/AeroJetCompiler/main.cpp
@@ -3,16 +3,15 @@
 #include "CodeGen/LLVM/IRCodeGen.hpp"
 #include "cxxopts.hpp"
 #include "Environment.hpp"
+#include "ErrorCodes.hpp"
 #include "fmt/format.h"
 
 int main(int argc, char** argv)
 {
-    static constexpr AeroJet::i4 EXIT_CODE_OK           = 0;
-    static constexpr AeroJet::i4 EXIT_CODE_INVALID_ARGS = 1;
-
     static constexpr auto TARGET_COMMAND_LINE_OPTION     = "target";
     static constexpr auto CLASS_PATH_COMMAND_LINE_OPTION = "classpath";
     static constexpr auto MAIN_CLASS_COMMAND_LINE_OPTION = "mainclass";
+    static constexpr auto OUTPUT_COMMAND_LINE_OPTION = "output";
 
     static constexpr auto JAR_FILE_EXTENSION        = ".jar";
     static constexpr auto JAVA_CLASS_FILE_EXTENSION = ".class";
@@ -21,15 +20,26 @@ int main(int argc, char** argv)
     options.add_options()
         (TARGET_COMMAND_LINE_OPTION, "target jar or java class file", cxxopts::value<std::string>())
         (CLASS_PATH_COMMAND_LINE_OPTION,"class path",cxxopts::value<std::string>())
-        (MAIN_CLASS_COMMAND_LINE_OPTION, "main class", cxxopts::value<std::string>());
+        (MAIN_CLASS_COMMAND_LINE_OPTION, "main class", cxxopts::value<std::string>())
+        (OUTPUT_COMMAND_LINE_OPTION, "base output directory", cxxopts::value<std::string>());
 
     cxxopts::ParseResult parseResult = options.parse(argc, argv);
+
+    AeroJet::Compiler::Environment::Builder compilerEnvironmentBuilder{};
+    compilerEnvironmentBuilder.output(parseResult[OUTPUT_COMMAND_LINE_OPTION].as<std::string>());
+
+    if(!parseResult.count(OUTPUT_COMMAND_LINE_OPTION))
+    {
+        fmt::print(stderr, "No output directory specified\n");
+        fmt::print("{}\n", options.help());
+        return AeroJet::Compiler::ErrorCodes::EXIT_CODE_INVALID_ARGS;
+    }
 
     if(!parseResult.count(TARGET_COMMAND_LINE_OPTION))
     {
         fmt::print(stderr, "No target file specified.\n");
         fmt::print("{}\n", options.help());
-        return EXIT_CODE_INVALID_ARGS;
+        return AeroJet::Compiler::ErrorCodes::EXIT_CODE_INVALID_ARGS;
     }
 
     std::string targetPathString = parseResult[TARGET_COMMAND_LINE_OPTION].as<std::string>();
@@ -40,11 +50,13 @@ int main(int argc, char** argv)
 
     std::filesystem::path targetPath = std::filesystem::path(targetPathString);
 
-    AeroJet::Compiler::Environment::Builder compilerEnvironmentBuilder{};
     compilerEnvironmentBuilder.classPath(cp);
     if(targetPath.extension() == JAVA_CLASS_FILE_EXTENSION)
     {
-        compilerEnvironmentBuilder.mainClass(targetPath);
+        compilerEnvironmentBuilder.mainClass(AeroJet::Compiler::MainClassStorage{
+            AeroJet::Compiler::MainClassStorage::Kind::CLASS_FILE,
+            targetPath
+        });
     }
     else if(targetPath.extension() == JAR_FILE_EXTENSION)
     {
@@ -52,18 +64,21 @@ int main(int argc, char** argv)
         {
             fmt::print(stderr, "No main class specified.\n");
             fmt::print("{}\n", options.help());
-            return EXIT_CODE_INVALID_ARGS;
+            return AeroJet::Compiler::ErrorCodes::EXIT_CODE_INVALID_ARGS;
         }
 
-        compilerEnvironmentBuilder.mainClass(parseResult[MAIN_CLASS_COMMAND_LINE_OPTION].as<std::string>());
+        compilerEnvironmentBuilder.mainClass({
+            AeroJet::Compiler::MainClassStorage::Kind::ARCHIVE,
+            fmt::format("{}:{}", targetPath.string(), parseResult[MAIN_CLASS_COMMAND_LINE_OPTION].as<std::string>())
+        });
     }
     else
     {
         fmt::print(stderr, "Target file has unknown file format {}.\n", targetPath.extension().string());
-        return EXIT_CODE_INVALID_ARGS;
+        return AeroJet::Compiler::ErrorCodes::EXIT_CODE_INVALID_ARGS;
     }
 
-    std::unique_ptr<AeroJet::Compiler::IRCodeGen> codeGen =
-        std::make_unique<AeroJet::Compiler::IRCodeGen>(compilerEnvironmentBuilder.build());
+    std::unique_ptr<AeroJet::Compiler::LLVM::IRCodeGen> codeGen =
+        std::make_unique<AeroJet::Compiler::LLVM::IRCodeGen>(compilerEnvironmentBuilder.build());
     return codeGen->run();
 }
