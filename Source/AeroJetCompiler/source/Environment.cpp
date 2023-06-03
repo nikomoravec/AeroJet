ba39340aecf4e2deb0eb20203d46b9216fe32738
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2023-06-03 02:03:11 +0200

    license added

--- Source/AeroJetCompiler/source/Environment.cpp
+++ Source/AeroJetCompiler/source/Environment.cpp
@@ -1,3 +1,27 @@
+/*
+ * Environment.cpp
+ *
+ * Copyright © 2023 AeroJet Developers. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "Environment.hpp"
 
 namespace AeroJet::Compiler
@@ -49,9 +73,7 @@ namespace AeroJet::Compiler
         return m_output;
     }
 
-    MainClassStorage::MainClassStorage() : m_kind(Kind::CLASS_FILE), m_path()
-    {
-    }
+    MainClassStorage::MainClassStorage() : m_kind(Kind::CLASS_FILE), m_path() {}
 
     MainClassStorage::MainClassStorage(MainClassStorage::Kind kind, std::filesystem::path path) :
         m_kind(kind), m_path(std::move(path))
