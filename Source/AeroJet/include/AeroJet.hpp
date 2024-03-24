5a1b6f58cbf81b844f920107184f8fe486f9cc21
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2024-03-24 16:35:57 +0100

    assertion macro that can throw an exception

--- Source/AeroJet/include/AeroJet.hpp
+++ Source/AeroJet/include/AeroJet.hpp
@@ -24,6 +24,7 @@
 
 #pragma once
 
+#include "Assertion.hpp"
 #include "Exceptions/FileNotFoundException.hpp"
 #include "Exceptions/IncorrectAttributeTypeException.hpp"
 #include "Exceptions/OperationNotSupportedException.hpp"
